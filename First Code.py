b65b3b0f156c4302f187ca19221e7320a6000171
Mudassar Shahzad <88019345+mudassarsos@users.noreply.github.com>
2026-05-31 21:29:01 +1200

    Add files via upload

---
+++ First Code.py
@@ -0,0 +1,21 @@
+print("Hello, World!    Welcome to Python programming.  Let's learn together.")
+a=3
+my_name="Mudassar"
+full_message='My name is : {}'.format(my_name)
+
+if a==3:
+    print(full_message)
+else:
+    print("test")
+from random import randint
+print(randint(1,100))
+
+a=5
+b=10
+c=True
+d=False
+print(a+b)
+print(c)
+print(d)
+print(not d)
+
